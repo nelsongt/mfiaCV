cbf4560b3e966a35c950401d686c9319fff0e86a
nelsongt <gtn1425@rit.edu>
2020-11-04 15:38:19 -0500

    Increased SNR, updated comments

--- CV_Main.m
+++ CV_Main.m
@@ -2,39 +2,39 @@
 % Set sample info
 sample.user = 'George';
 sample.material = 'In0.53Ga0.47As';
-sample.name = 'GAP500-Stage1';
+sample.name = 'GPD2-1MeVF2';
 sample.area = '0.196';  % mm^2
-sample.comment = 'Post1180KAnneal';
-sample.save_folder = strcat('.\data\',sample.name,'_',datestr(now,'mm-dd-yyyy-HH-MM-SS'));  % folder data will be saved to, uses timecode so no overwriting happens
+sample.comment = 'Long integration';
 
 % Set YSpec experiment parameters
-mfia.sample_time = 5;    % sec, length to sample each temp point, determines speed of scan and SNR
-bias_start = 0.1;      % Hz, start lock in AC frequency, GN suggests ~100Hz
-bias_final = 2.5;      % Hz, final frequency, GN suggests 5MHz (MFIA limit)
-bias_step = 0.01;      % Frequency step size on the log-scale
+mfia.sample_time = 7;    % sec, length to sample each temp point, determines speed of scan and SNR
+bias_start = 0.1;      % V, start bias
+bias_final = -4;      % V, final bias
+bias_step = 0.02;      % V, Bias step size
 
 % Set temperature parameters
-temp_init = 150;        % K, Initial temperature
-temp_step = 10;         % K, Temperature step size
-temp_final = 150;        % K, Ending temperature
-temp_idle = 180;        % K, Temp to set after experiment is over
+temp_init = 250;        % K, Initial temperature
+temp_step = 150;         % K, Temperature step size
+temp_final = 100;        % K, Ending temperature
+temp_idle = 250;        % K, Temp to set after experiment is over
 temp_stability = 0.05;   % K, Sets how stable the temperature point must be (set point +- stability)
 time_stability = 20;    % s, How long must temperature be stable before collecting data, useful if sample lags temperature or if PID settings are overshooting beyond the stability criteria above
 
 % Set MFIA Parameters
-mfia.time_constant = 2.4e-3;  % us, lock in time constant, GN suggests 2.4e-3
-mfia.pulse_height = 0.0;      % V, has to be zero for YSpec, don't change this
+mfia.time_constant = 2.0e-2;  % s, lock in time constant, GN suggests ~2.4e-2
+mfia.pulse_height = 0.0;      % V, has to be zero for CV, don't change this
 mfia.ac_ampl = 0.080;           % V, lock in AC amplitude, GN suggests ~100 mV for good SNR
-mfia.sample_rate = 107143;     % Hz, sampling rate Hz, for Y_Spec use 53571 or 107143
-mfia.ac_freq = 1e6;           % Hz, not used for YSpec
-mfia.ss_bias = bias_start;           % Hz, not used for YSpec
-mfia.full_period = 0.150;     % s, not used for YSpec
-mfia.trns_length = 0.150;     % s, not used for YSpec
-mfia.pulse_width = 0.000;     % s, not used for YSpec
+mfia.sample_rate = 107143;     % Hz, sampling rate Hz, for CV use 53571 or 107143
+mfia.ac_freq = 1e6;           % Hz, not used for CV
+mfia.ss_bias = bias_start;           % Hz, not used for CV
+mfia.full_period = 0.150;     % s, not used for CV
+mfia.trns_length = 0.150;     % s, not used for CV
+mfia.pulse_width = 0.000;     % s, not used for CV
 mfia.irange = 0.0001;         % A, current range for MFIA  
 
 
 % Setup PATH
+sample.save_folder = strcat('.\data\',sample.name,'_',datestr(now,'mm-dd-yyyy-HH-MM-SS'));  % folder data will be saved to, uses timecode so no overwriting happens
 addpath(genpath('.\lakeshore'))		% point to lakeshore driver
 addpath(genpath('.\LabOneMatlab'))  % point to LabOneMatlab drivers
 ziAddPath % ZI instrument driver load
@@ -58,12 +58,8 @@ while current_num <= steps
     SET_TEMP(current_temp,temp_stability,time_stability); % Wait for lakeshore to reach set temp;
     for i=1:length(biases)
         mfia.ss_bias = biases(i);
-        %if freqs(i) < 9900000
-        %    mfia.irange = 0.0001;
-        %elseif freqs(i) > 9900000
-        %    mfia.irange = 0.001;
-        %end
         [timeStamp, sampleCap, sampleRes] = MFIA_CAPACITANCE_POLL(device,mfia);
+        pause(0.5);
         avg_R(i) = mean(sampleRes);
         avg_C(i) = mean(sampleCap);
         cprintf('blue', 'Current bias: %d \n',biases(i));
